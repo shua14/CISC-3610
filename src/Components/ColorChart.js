64e2932e4bcd7bb7a6cf597e9eac448e014e722a
Shua <Shua@MacBook-Air-4.fios-router.home>
2022-11-13 02:05:44 -0500

    finshed lab

--- src/Components/ColorChart.js
+++ src/Components/ColorChart.js
@@ -1,28 +1,45 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function ColorChart(){
-
-    // const [fruit, setFruit] = useState([]);
-
     useEffect(() => {
         const fruit = [ 
-            {"name":"Strawberry", "quantity":15, "color":"red"},
-            {"name":"Peach", "quantity":10, "color":"orange"},
-            {"name":"Banana", "quantity":25, "color":"yellow"},
-            {"name":"Pear", "quantity":10, "color":"green"},
-            {"name":"grape", "quantity":20, "color":"purple"}
+            {name:"Strawberry", quantity:15, color:"red"},
+            {name:"Peach", quantity:10, color:"orange"},
+            {name:"Banana", quantity:25, color:"yellow"},
+            {name:"Pear", quantity:10, color:"green"},
+            {name:"Grape", quantity:20, color:"purple"}
         ];
 
         const result = JSON.stringify(fruit)
         const obj = JSON.parse(result)
 
-        console.log(obj[0].name)
-
         const c = document.getElementById("myCanvas");
         const context = c.getContext("2d");
-        context.fillStyle = '#90EE90'
+        context.fillStyle = 'grey'
         context.fillRect(0, 0, 1000, 500)
+        context.stroke();
 
+        context.font = "30px Arial";
+        context.fillStyle = "black";
+        context.fillText("Jay Luxenberg", 850, 80)
+
+        let x = 325;
+        for(let i = 0; i < fruit.length; i++){
+            context.strokeStyle = obj[i].color;
+            context.lineWidth = 70;
+            context.beginPath();
+            context.moveTo(x, 325 - (obj[i].quantity * 10));
+            context.lineTo(x, 400);
+            context.stroke();
+
+            context.font = "20px Arial";
+            context.fillStyle = "brown";
+            context.textAlign = 'center';
+            context.fillText(obj[i].name, x, 425);
+            x += 90;
+            context.stroke();
+        }
     }, [])
     
     return(
@@ -32,10 +49,11 @@ export default function ColorChart(){
                 id="myCanvas"
                 width="1000"
                 height="500"
-                style={{ border: "1px solid #90EE90" }}
+                style={{ border: "1px solid grey" }}
             >
                 Your browser does not support the HTML canvas tag.
             </canvas>
+            <div><Link to="/">Back Home</Link></div>
         </>
     );
 }
